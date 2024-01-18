a05e07661ef31f340d772090d7a764cdc13a023c
Andrew Dinh <andrewdinh74@yahoo.com>
2024-01-18 07:08:05 -0800

    Add docker-compose file to include volumes info for convenience

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload unless you are Andrew
+          Edit <code>src/App.js</code> and save to reload unless you are not interested
         </p>
         <a
           className="App-link"
